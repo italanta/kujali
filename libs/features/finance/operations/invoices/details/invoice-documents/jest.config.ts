3ccd5a04c211fdda888443482538883d31eebc66
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-03-26 15:17:12 +0300

    :wrench: update import paths

--- libs/features/finance/operations/invoices/details/invoice-documents/jest.config.ts
+++ libs/features/finance/operations/invoices/details/invoice-documents/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'features-finance-invoices-details-invoice-documents',
-  preset: '../../../../../../jest.preset.js',
+  preset: '../../../../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -10,7 +10,7 @@ export default {
     },
   },
   coverageDirectory:
-    '../../../../../../coverage/libs/features/finance/invoices/details/invoice-documents',
+    '../../../../../../../coverage/libs/features/finance/invoices/details/invoice-documents',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
