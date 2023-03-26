f2b3ebd3fd64176b870b25dd1659cd319d54c533
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-03-26 15:35:00 +0300

    :truck: moved operations main into finance

--- libs/features/finance/operations/main/jest.config.ts
+++ libs/features/finance/operations/main/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'features-operations-main',
-  preset: '../../../../jest.preset.js',
+  displayName: 'features-finance-operations-main',
+  preset: '../../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
@@ -9,7 +9,8 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../../coverage/libs/features/operations/main',
+  coverageDirectory:
+    '../../../../../coverage/libs/features/finance/operations/main',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
