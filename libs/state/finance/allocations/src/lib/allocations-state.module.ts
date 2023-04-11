47ccb3745b406b2896844dbe421476e5e956b5d3
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-04-11 08:10:10 +0300

    :horse: added invoice allocations store

--- libs/state/finance/allocations/src/lib/allocations-state.module.ts
+++ libs/state/finance/allocations/src/lib/allocations-state.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaymentsAllocationsStore } from './stores/payments-allocations.store';
+import { InvoicesAllocationsStore } from './stores/invoices-allocations.store';
 
 @NgModule({
   imports: [CommonModule],
@@ -11,7 +12,8 @@ export class AllocationsStateModule {
     return {
       ngModule: AllocationsStateModule,
       providers: [
-        PaymentsAllocationsStore
+        PaymentsAllocationsStore,
+        InvoicesAllocationsStore
       ]
     };
   }
