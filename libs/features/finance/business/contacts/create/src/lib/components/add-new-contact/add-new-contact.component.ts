2b6ad9f9fdb248ac3cd381a99aadde578120d700
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-04-19 08:35:08 +0300

    :fire: remove international phone since packs are not up to date

--- libs/features/finance/business/contacts/create/src/lib/components/add-new-contact/add-new-contact.component.ts
+++ libs/features/finance/business/contacts/create/src/lib/components/add-new-contact/add-new-contact.component.ts
@@ -2,8 +2,6 @@ import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular
 import { FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { TranslateService } from '@ngfi/multi-lang';
-
 @Component({
   selector: 'add-new-contact',
   templateUrl: './add-new-contact.component.html',
@@ -19,21 +17,9 @@ export class AddNewContactComponent implements OnInit {
 
   lang: 'fr' | 'en' | 'nl';
   
-  constructor(private _translateService: TranslateService,
-              @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
-    this.lang = this._translateService.initialise();
-  }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void { }
-  
-  setLang(lang: 'en' | 'fr' | 'nl') {
-    this._translateService.setLang(lang);
-  }
-
-  translTe(text) {
-    return this._translateService.translate(text);
-  }
 
   onCountryChange(country: any) {
     this.countryCodeEvent.emit(country.dialCode)
