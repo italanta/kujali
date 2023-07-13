9d1979531112b3e38999159737a255b85ec8a34c
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-07-13 15:49:23 +0300

    :recycle: dashboard refactor

--- libs/features/dashboard/main/src/lib/pages/dashboard/dashboard.page.ts
+++ libs/features/dashboard/main/src/lib/pages/dashboard/dashboard.page.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+import { SubSink } from 'subsink';
+
+import { Observable, tap } from 'rxjs';
 
 import { User } from '@iote/bricks';
 import { UserService } from '@ngfi/angular';
+
 import { MetabaseService } from '@app/state/organisation';
 
 @Component({
@@ -10,42 +14,52 @@ import { MetabaseService } from '@app/state/organisation';
   templateUrl: './dashboard.page.html',
  styleUrls: ['./dashboard.page.scss']
 })
-export class DashboardPageComponent
+export class DashboardPageComponent implements OnInit
 {
+  private _sbS = new SubSink();
+
   user$: Observable<User>;
-  loading = true;
 
   iframeUrl: string;
+  generatingMetabaseLink: boolean = false
 
   constructor(_userService: UserService<User>,
               private _mbService: MetabaseService)
   {
     this.user$ = _userService.getUser();
-    
-    this.user$.subscribe(user => 
-                          {
-                            this.generateMetabaseLink()
-                            this.loading = false;
+  }
 
-                            // if(!!user.profile.metabaseUrl)
-                            // {
-                            //   this.iframeUrl = user.profile.metabaseUrl;
-                            //   this.loading = false;
-                            // }
-                            // else
-                            // {
-                            //   this.generateMetabaseLink()
-                            // }
-                          })
+  ngOnInit(): void {
+    this.generateMetabaseLink()
+  }
+
+  assignUserMetabaseUrl() {
+    this._sbS.sink = this.user$.subscribe(user => 
+      {
+        // if(!!user.profile.metabaseUrl)
+        // {
+        //   this.iframeUrl = user.profile.metabaseUrl;
+        //   this.loading = false;
+        // }
+        // else
+        // {
+        //   this.generateMetabaseLink()
+        // }
+      })
   }
 
   //Call backend fn that generates metabase link
   generateMetabaseLink()
   {
-    this._mbService.getMetabaseLink().subscribe(res => {
-      this.loading = false;
-      this.iframeUrl = res;
-    });
+    this.generatingMetabaseLink = true;
+
+    this._mbService.getMetabaseLink()
+                            .pipe(tap((metabaseLink) => {
+                                    if (metabaseLink) {
+                                      this.iframeUrl = metabaseLink;
+                                      this.generatingMetabaseLink = false;
+                                    }}))
+                            .subscribe();
   }
 
 }
