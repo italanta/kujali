51edcaa2f5061f37d5e10c2e9949e9dbd37dbf1c
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-07-14 10:33:52 +0300

    :wrench: added page titles

--- apps/kujali/src/app/app.component.ts
+++ apps/kujali/src/app/app.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'Kujali';
+
+  constructor(private _router$$: Router,
+              private _activatedRoute$$: ActivatedRoute,
+              private _title$$: Title
+  ) {
+    this._router$$.events.pipe(filter(event => event instanceof NavigationEnd),
+                               map(() => this.getActivePageName()))
+                          .subscribe((data: string) => {
+                          if (data) 
+                            this._title$$.setTitle('Kujali - '+ data);
+                          });
+  }
+
+  getActivePageName() {
+    let child = this._activatedRoute$$.firstChild;
+    while (child) {
+      if (child.firstChild) {
+        child = child.firstChild;
+      } else if (child.snapshot.data && child.snapshot.data['title']) {
+        return child.snapshot.data['title'];
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
 }
--- apps/kujali/src/app/app.routing.module.ts
+++ apps/kujali/src/app/app.routing.module.ts
@@ -15,38 +15,45 @@ export const APP_ROUTES: Route[] = [
   {
     path: 'auth',
     loadChildren: () => import('@app/features/auth/login').then(m => m.AuthModule),
+    data: { title: 'Authentication' }
   },
 
   {
     path: 'orgs',
     loadChildren: () => import('libs/features/organisation/main/src/lib/organisation.module').then(m => m.OrganisationModule),
+    data: { title: 'Organisation' }
   },
 
   {
     path: 'home',
     loadChildren: () => import('@app/features/dashboard/main').then(m => m.DashboardModule),
+    data: { title: 'Dashboard' },
     canActivate: [IsLoggedInGuard]
   },
   {
     path: 'business',
     loadChildren: () => import('@app/features/finance/business/main').then(m => m.FinanceBusinessModule),
+    data: { title: 'Business' },
     canActivate: [IsLoggedInGuard]
   },
   {
     path: 'operations',
     loadChildren: () => import('@app/features/finance/operations/main').then(m => m.OperationsModule),
+    data: { title: 'Operations' },
     canActivate: [IsLoggedInGuard]
   },
 
   {
     path: 'budgets',
     loadChildren: () => import('@app/features/budgetting/budgets').then(m => m.FinancialPlanningModule),
+    data: { title: 'Budgets' },
     canActivate: [IsLoggedInGuard]
   },
 
   {
     path: 'settings',
     loadChildren: () => import('@app/features/settings/main').then(m => m.SettingsModule),
+    data: { title: 'Settings' },
     canActivate: [IsLoggedInGuard]
   },
 ];
