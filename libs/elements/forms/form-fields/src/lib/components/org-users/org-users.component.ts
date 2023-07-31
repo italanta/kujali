ef98e16973b1441ba2109e382f676d9264142e9d
ianodhiambo9 <ianodhiambo9@gmail.com>
2023-07-31 08:33:00 +0300

    :wrench: edited orgs comp for restricted access

--- libs/elements/forms/form-fields/src/lib/components/org-users/org-users.component.ts
+++ libs/elements/forms/form-fields/src/lib/components/org-users/org-users.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { tap } from 'rxjs';
 import { SubSink } from 'subsink';
 
 import { KuUser } from '@app/model/common/user';
@@ -15,25 +16,33 @@ import { OrganisationService } from '@app/state/organisation';
 export class OrgUsersFormField implements OnInit {
   private _sbS = new SubSink();
 
+  @Input() restriction: boolean = false;
   @Input() parentForm: FormGroup;
 
   orgUsers: KuUser[];
   filteredUsers: KuUser[];
 
+  fetchedUsers: boolean = false;
+
   constructor(private _orgsService$$: OrganisationService) 
-  { }
+  {    
+  }
 
   ngOnInit(): void {
     this._getOrgUsers()
   }
 
   private _getOrgUsers() {    
-    this._sbS.sink = this._orgsService$$
+    this._orgsService$$
       .getOrgUsersDetails()
-      .subscribe((users) => {
+      .pipe(tap((users) => {
         this.orgUsers = users;
         this.filteredUsers = this.orgUsers.slice();
-      });
+        this.fetchedUsers = true;
+      })).subscribe();
   }
 
+  compareFn(c1, c2): boolean {
+    return c1.id && c2.id ? c1.id === c2.id :c1 === c2;
+  }
 }
